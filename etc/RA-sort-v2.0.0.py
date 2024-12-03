bcf91e8d9259fd5b531f5b00d35aa055c39f8768
Cody <costa.cody7@gmail.com>
2024-12-03 14:48:27 -0800

    touched up RA-sort-v2.0.0

--- etc/RA-sort-v2.0.0.py
+++ etc/RA-sort-v2.0.0.py
@@ -15,63 +15,54 @@ Planned changes:
 
 import os, shutil, glob
 
-scan_types = ['CETUS', 'COG', 'EPSM', 'SL']
-thresholds = ['65', '75', '85']
-
 # RA file storage
 root_dir = os.getcwd()
 
 # loop through each file and find both scan type and threshold
 for RA in glob.glob(f'{root_dir}/RA*.txt'):
-    file_name = os.path.splitext(RA)[0]
-    if not os.path.splitext(RA)[1] or os.path.splitext(RA)[1] != '.txt':
-        continue
-
-    if 'CETUS' in file_name:
+    # Tested and working
+    if 'CETUS' in RA:
         if not os.path.exists(f'{root_dir}/CETUS'):
             os.mkdir(f'{root_dir}/CETUS')
+            print(f'{root_dir}\\CETUS')
         shutil.move(RA, f'{root_dir}/CETUS')
         continue
 
-    if 'SPICA' in file_name:
-        if 'MC' in file_name:
-            if '-DDB-' in file_name or '-D2D-' in file_name:
-                threshold_idx_start = file_name.index('dbdd') + 4
-                threshold = file_name[threshold_idx_start: threshold_idx_start + 2]
+    if 'SPICA' in RA:
+        # Still untested, need MC files to test
+        if 'MC' in RA:
+            if '-DDB-' in RA or '-D2D-' in RA:
+                threshold_idx_start = RA.index('dbdd') + 4
+                threshold = RA[threshold_idx_start: threshold_idx_start + 2]
                 if threshold == 'sl':
-                    threshold = file_name[threshold_idx_start + 2: threshold_idx_start + 4]
+                    threshold = RA[threshold_idx_start + 2: threshold_idx_start + 4]
 
-                if 'COG' in file_name or '260C-' in file_name or '320C-' in file_name or '400C-' in file_name:
+                if 'COG' in RA or '260C-' in RA or '320C-' in RA or '400C-' in RA:
                     scan_type = 'COG'
-                elif 'EPSM' in file_name or '260E-' in file_name or '320E-' in file_name or '400E-' in file_name:
+                elif 'EPSM' in RA or '260E-' in RA or '320E-' in RA or '400E-' in RA:
                     scan_type = 'EPSM'
             
-            elif '-SL-' in file_name:
+            elif '-SL-' in RA:
                 scan_type = 'SL'
-                threshold_idx_start = file_name.index('-P') - 2
-                threshold = file_name[threshold_idx_start: threshold_idx_start + 2]
-
+                threshold_idx_start = RA.index('-P') - 2
+                threshold = RA[threshold_idx_start: threshold_idx_start + 2]
+        
+        # Tested and working
         else:
-            threshold_idx_start = file_name.index('-P') - 2
-            threshold = file_name[threshold_idx_start: threshold_idx_start + 2]
+            threshold_idx_start = RA.index('-P') - 2
+            threshold = RA[threshold_idx_start: threshold_idx_start + 2]
 
-            if 'COG' in file_name or '260C-' in file_name or '320C-' in file_name or '400C-' in file_name:
+            if 'COG' in RA or '260C-' in RA or '320C-' in RA or '400C-' in RA:
                 scan_type = 'COG'
-            elif 'EPSM' in file_name or '260E-' in file_name or '320E-' in file_name or '400E-' in file_name:
+            elif 'EPSM' in RA or '260E-' in RA or '320E-' in RA or '400E-' in RA:
                 scan_type = 'EPSM'
-                if 'UXRsl' in file_name:
+                if 'UXRsl' in RA:
                     scan_type = 'SL'
                     
-
-
-    destination = f'{root_dir}/{scan_type}/{threshold}'
-    print(destination)
-
-    # only create unique destination folders
-    if not os.path.exists(f'{root_dir}/{scan_type}'):
-        os.mkdir(f'{root_dir}/{scan_type}')
+    destination = f'{root_dir}\\{scan_type}\\{threshold}'
     if not os.path.exists(destination):
-        os.mkdir(destination)
+        os.makedirs(destination)
+        print(destination)
 
     # move file
     shutil.move(RA, destination)
